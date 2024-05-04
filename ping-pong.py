29490ff564c7beb4bc1229bf89c3b47710403f7d
sveta1397 <sveta1397@mail.ru>
2024-05-04 13:34:12 +0300

    Добавили спрайты

--- ping-pong.py
+++ ping-pong.py
@@ -7,6 +7,35 @@ window = display.set_mode((win_width, win_height))
 
 back = (200, 255, 255) 
 window.fill(back)
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height)) #вместе 55,55 - параметры
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 80) 
+racket2 = Player('racket.png', 620, 200, 4, 50, 80)
+ball = GameSprite('ball.png', 200, 200, 4, 50, 30)
+
 game = True #флаг сбрасывается кнопкой закрытия окна
 finish = False
 while game:
@@ -15,5 +44,12 @@ while game:
             game = False
     if not finish:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
         display.update()
     time.delay(50)
