61773c5be4655f91ff6667431f4bbe8b11cea812
sveta1397 <sveta1397@mail.ru>
2024-05-11 13:06:50 +0300

    добавили движение мяча

--- ping-pong.py
+++ ping-pong.py
@@ -4,14 +4,13 @@ win_width = 700
 win_height = 500
 display.set_caption("PING_PONG")
 window = display.set_mode((win_width, win_height))
-
 back = (200, 255, 255) 
 window.fill(back)
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (width, height)) #вместе 55,55 - параметры
+        self.image = transform.scale(image.load(player_image), (width, height)) 
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -36,6 +35,13 @@ racket1 = Player('racket.png', 30, 200, 4, 50, 80)
 racket2 = Player('racket.png', 620, 200, 4, 50, 80)
 ball = GameSprite('ball.png', 200, 200, 4, 50, 30)
 
+speed_y = 3
+speed_x = 3
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
 game = True #флаг сбрасывается кнопкой закрытия окна
 finish = False
 while game:
@@ -46,6 +52,22 @@ while game:
         window.fill(back)
         racket1.update_l()
         racket2.update_r()
+        
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
 
         racket1.reset()
         racket2.reset()
